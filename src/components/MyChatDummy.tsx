96e01d4ebf361098bb86b8c6e93aabd108de9905
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-04-11 05:42:52 +0900

    중간저장

---
+++ src/components/MyChatDummy.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const MyChatDummy = [
+  { id: 1, content: "ㅎㅇ", date: "2022-01-01" },
+  { id: 2, content: "ㅋㅋ", date: "2022-01-02" },
+  { id: 3, content: "ㄱㄱ", date: "2022-01-01" },
+  { id: 4, content: "ㄴㄴ", date: "2022-01-02" },
+];
+
+export default MyChatDummy;
+
+{
+  /* <button
+onClick={() => {
+  axios
+    .get("https://codingapple1.github.io/shop/data2.json")
+    .then((res) => {
+      console.log(res.data);
+
+      let copy = [...shoes, ...res.data];
+
+      setShoes(copy);
+    })
+    .catch(() => {
+      console.log("에러뜸");
+    });
+}}
+>
+그버튼
+</button> */
+}
