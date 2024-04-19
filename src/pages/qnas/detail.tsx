78e2a1e53216dd4b50c41a78920080be546855e3
ybmin1 <yebin.min1@gmail.com>
2024-04-19 18:32:46 +0900

    발표위한 목데이터 추가

--- src/pages/qnas/detail.tsx
+++ src/pages/qnas/detail.tsx
@@ -64,17 +64,17 @@ export default function QnaDetailPage() {
   const { postId } = useParams();
 
   const mockData: Question = {
-    questionId: 6,
+    questionId: 1,
     questionerId: 1234567,
     questioner: "메추리",
     profileImg: "anonymous.png",
-    title: "질문입니다.",
+    title: "flutter 공부하고 있는 4학년 재학생입니다",
     content:
-      "현재 중소기업에서 데이터분석가로 일하고 있습니다. 하지만 데이터분석일은 하지않고 거의 파이썬 개발일, 프로젝트관리 정도만 하고 있습니다. 국가과제를 주로 하는 회사의 특성상 제안서가 떨어지면 그마저도 일이 없습니다. 복지도 줄어드는 것이 보입니다. 지금 2년차고, 회사에 데이터 직무 선배는 커녕 개발자도 없습니다. 이직이 정말 간절한데, 회사에서 배운게 없어 포폴 할만한 것도 없고, 코테도 점점 자신이 없어 집니다. 배우고 성장하는게 중요한 사람인데, 먼거리 출퇴근할 때마다, 너무 괴롭고 눈물이 납니다.",
+      "안녕하세요, 현재 플러터를 공부하고 있는 4학년 소프트웨어학과 학생입니다. 3학년때 처음 플러터를 알게 되었고 공부에 흥미가 생겨서 현재까지도 하고있습니다. 희망진로가 없었던 터라 현재 공부하고 있는 플러터로 취업을 할까 고민중입니다. 플러터를 공부하면서 동시에 3학년 2학기에 팀프로젝트로 안드로이드 스튜디오를 이용한 사이드프로젝트를 만든 경험이 있습니다. 하지만 네이티브 앱을 작업하면서 확실히 플러터의 매력에 더 빠지게 되었습니다. 이런 상황에서 플러터만 공부해서 취업하는 것이 가능할까요? 아니면 네이티브 앱을 주로 공부하면서 플러터 프로젝트를 병행해야 할까요?",
     createdAt: "2024-04-12T18:10:46.656117",
     updatedAt: null,
-    likeCount: 0,
-    dislikeCount: 5,
+    likeCount: 3,
+    dislikeCount: 0,
     canDelete: false,
     answers: [
       {
@@ -83,45 +83,18 @@ export default function QnaDetailPage() {
         answererId: 1,
         profileImg: "anonymous.png",
         answerer: "나까무라답변자",
-        content: "뭐가 궁금하신데요?",
+        content:
+          "플러터만 공부하셔도 취업에는 크게 문제 없을거에요. 네이티브 코드를 아무것도 몰라도 플러터만 잘 알면 앱 개발이 가능하기 때문입니다. 그런데 플러터나 리액트 네이티브와 같은 하이브리드 앱을 개발하는 툴로 개발을 하다보면 패키지 오류라던지 등으로 인해 네이티브 코드를 건드려야 하는 상황이 오실거에요. 그래서 네이티브 코드를 이해하면 하이브리드앱을 개발할때도 도움이 되실거에요. 그리고 플러터 개발자를 구직하는 많은 채용공고들의 우대사항에 네이티브 앱 개발경험과 네이티브 모듈 작성 경험이 포함되어있는 경우가 많습니다. 그래서 저는 네이티브 코드도 어느정도 이해할 수 있도록 학습하시는 편이 좋을 것 같습니다?",
         createdAt: "2024-04-13T18:11:00.046106",
         updatedAt: "2024-04-13T18:11:00.046106",
-        likeCount: 1,
-        canDelete: false,
-        answerComments: [],
-        liked: true,
-      },
-
-      {
-        questionId: 6,
-        answerId: 2,
-        answererId: 1234567,
-        profileImg: "anonymous.png",
-        answerer: "메추리",
-        content: "죄송합니다. 쓰다가 말았네요.",
-        createdAt: "2024-04-13T18:11:00.822348",
-        updatedAt: "2024-04-13T18:11:00.822348",
-        likeCount: 0,
-        canDelete: false,
-        answerComments: [],
-        liked: false,
-      },
-      {
-        questionId: 6,
-        answerId: 3,
-        answererId: 1,
-        profileImg: "anonymous.png",
-        answerer: "나까무라복사품",
-        content: "??",
-        createdAt: "2024-04-13T18:11:01.470863",
-        updatedAt: "2024-04-13T18:11:01.470863",
-        likeCount: 0,
-        canDelete: false,
+        likeCount: 7,
+        canDelete: true,
         answerComments: [
           {
             commentId: 1,
-            content: "뭐라는걸까요 궁금",
-            commenterId: 1,
+            content:
+              "현재 졸업전에 정처기 취득과 코테공부 중에서 고민중에 있습니다. 둘 증 어느것에 플러터 취업에 더 도움되는지 여쭤봐도 될까요?",
+            commenterId: 1234567,
             commenter: "코멘터1",
             profileImage: "anonymous.png",
 
@@ -130,53 +103,40 @@ export default function QnaDetailPage() {
           },
           {
             commentId: 2,
-            content: "기다리고 있습니다",
+            content:
+              "저는 코테공부라고 생각합니다. 모든 자격증은 없는 것보단 좋은데 엄청나게 강점이 있다고 보기는 어렵다고 생각합니다",
             commenterId: 1,
-            commenter: "코멘터2",
+            commenter: "강병전",
             profileImage: "anonymous.png",
-
             createdAt: "2024-04-16T18:11:16.048811",
             canDelete: false,
           },
-          {
-            commentId: 3,
-            content: "작성자입니다. 해결되었습니다. 감사합니다.",
-            commenterId: 1234567,
-            commenter: "메추리",
-            profileImage: "anonymous.png",
-
-            createdAt: "2024-04-18T18:11:16.709377",
-            canDelete: false,
-          },
-          {
-            commentId: 4,
-            content: "여태 기다렸는데...",
-            commenterId: 1,
-            commenter: "코멘터4",
-            profileImage: "anonymous.png",
-
-            createdAt: "2024-04-20T18:11:17.23705",
-            canDelete: false,
-          },
-          {
-            commentId: 5,
-            content: "5~~~~~",
-            commenterId: 2,
-            commenter: "코멘터5",
-            profileImage: "anonymous.png",
-            createdAt: "2024-04-21T23:02:22.853126",
-            canDelete: false,
-          },
         ],
+        liked: true,
+      },
+
+      {
+        questionId: 8,
+        answerId: 2,
+        answererId: 12,
+        profileImg: "anonymous.png",
+        answerer: "aigoia",
+        content:
+          "최근에 플러터를 쓰는 회사에서 요청이 와서 이직중인데 수요가 있기는 한거 같습니다. 다만 저는 플러터 개발자는 아니고 게임 개발자이고 플러터는 곁가지로 살짝 맛만 본 수준입니다.",
+        createdAt: "2024-04-14T18:11:00.822348",
+        updatedAt: "2024-04-14T18:11:00.822348",
+        likeCount: 2,
+        canDelete: false,
+        answerComments: [],
         liked: false,
       },
     ],
-    liked: false,
+    liked: true,
     disliked: false,
   };
 
   //임의로넣은 상태. 추후 데이터 형식에 따라서 수정 필요
-  const [question, setQuestion] = useState<Question | null>(null);
+  const [question, setQuestion] = useState<Question | null>(mockData);
 
   //날짜, 시간 포멧팅
   const formattedDateYYMMDD = (dateString: string | undefined) => {
@@ -245,7 +205,6 @@ export default function QnaDetailPage() {
   };
 
   /////삭제 버튼 클릭시 /////
-
   const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옴
   //질문글 삭제 버튼 클릭시
   const questionDelBtnClickHandler = async () => {
@@ -274,8 +233,9 @@ export default function QnaDetailPage() {
     const getPost = async () => {
       try {
         const response = await axios.get(`${apiUrl}/api/question/${postId}`); // API 요청, postId는 usePrams사용해서 요청
+
         setQuestion(response.data); // 성공적으로 데이터를 가져왔을 때 상태 업데이트
-        console.log("Question 데이터 GET 성공");
+        console.log("Question 데이터 GET 성공", response.data);
       } catch (error) {
         console.log("Question 데이터 GET 실패:", error);
       }
